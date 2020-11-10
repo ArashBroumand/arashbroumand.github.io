aba60eb766116031fa4a10c3dde5235e9497e0ed
Arash Broumand <arash.broumand@gmail.com>
2020-11-10 19:20:52 +0330

    Add files via upload

--- accordionControl.js
+++ accordionControl.js
@@ -4,6 +4,7 @@ var i;
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
         this.classList.toggle("active");
+        console.log('hear you accordion');
         var panel = this.nextElementSibling;
         if (panel.style.maxHeight) {
             panel.style.maxHeight = null;
